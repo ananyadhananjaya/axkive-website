33810f827c02a873868714c064a4e1f93ca7c614
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-05-04 11:49:11 +0530

    vercel deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73504b41442d527e"; }
+/******/ 		__webpack_require__.h = function() { return "b4c0c4589f39886b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
