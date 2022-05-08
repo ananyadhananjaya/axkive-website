9ba058f28f66dc956a464691c0e4ce5a7f75bf40
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-05-08 11:51:59 +0530

    fix(vercel): fail deploy

--- pages/works.js
+++ pages/works.js
@@ -5,8 +5,8 @@ import Section from '../components/section'
 
 import thumbStatic from '../public/images/works/static-portfolio-css.png'
 import thumbNasa from '../public/images/works/nasa-api-static-site.png'
-import thumbLogs from "../public/images/works/logs-firebase.png"
-import thunbKnowMore from "../public/images/works/nextjs-static-portfolio.png"
+import thumbLogs from '../public/images/works/logs-firebase.png'
+import thunbKnowMore from '../public/images/works/nextjs-static-portfolio.png'
 
 const Works = () => {
   return (
@@ -15,7 +15,7 @@ const Works = () => {
         <Heading fontSize={22} mb={4}>
           Works
         </Heading>
-        <SimpleGrid columns={[1,1,2]} spacing={10}>
+        <SimpleGrid columns={[1, 1, 2]} spacing={10}>
           <Section>
             <WorkGridItem
               id="static-portfolio"
@@ -27,20 +27,36 @@ const Works = () => {
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem id="nasa-apod" title={'Apod'} thumbnail={thumbNasa} href={'https://api-ax-98.web.app/ '}>
+            <WorkGridItem
+              id="nasa-apod"
+              title={'Apod'}
+              thumbnail={thumbNasa}
+              href={'https://api-ax-98.web.app/ '}
+            >
               An Application that gives us Astronomy Picture of the Day and a
               small description on the Image.
             </WorkGridItem>
           </Section>
           <Section>
-              <WorkGridItem id="logs-app" title={"Logs"} thumbnail={thumbLogs} href={'https://logs-tawny.vercel.app/'}>
-                An Application where we can log. No login or account creation required.
-              </WorkGridItem>
+            <WorkGridItem 
+              id="logs-app"
+              title={'Logs'}
+              thumbnail={thumbLogs}
+              href={'https://logs-tawny.vercel.app/'}
+            >
+              An Application where we can log. No login or account creation
+              required.
+            </WorkGridItem>
           </Section>
           <Section>
-              <WorkGridItem id="know-more" title={"know-more"} thumbnail={thunbKnowMore}  href={'https://know-more-about-me.vercel.app/'}>
-                  A NextJs Application to know more about me.
-              </WorkGridItem>
+            <WorkGridItem
+              id="know-more"
+              title={'know-more'}
+              thumbnail={thunbKnowMore}
+              href={'https://know-more-about-me.vercel.app/'}
+            >
+              A NextJs Application to know more about me.
+            </WorkGridItem>
           </Section>
         </SimpleGrid>
       </Container>
@@ -50,4 +66,3 @@ const Works = () => {
 
 export default Works
 export { getServerSideProps } from '../components/chakra'
-
